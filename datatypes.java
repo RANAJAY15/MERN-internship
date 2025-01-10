f0b392b3147005771fe930440692f6bf3e899f7f
unknown <ranajay.s2021@gamil.com>
2025-01-10 09:04:52 +0530

    push

---
+++ datatypes.java
@@ -0,0 +1,50 @@
+var a = [1,"two","three"]
+console.log(a)
+
+var b = [{
+    "name" : "jp",
+    "class" : "CSE A"
+},
+{
+    "name" : "aji",
+    "class" : "CSE A"
+},
+{
+    "name" : "Dharanish",
+    "class" : "CSE A"
+}]
+b.forEach((person) => {
+    console.log(person.name,person.class)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
